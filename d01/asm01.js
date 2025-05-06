4ee55add989cbaf4d4082fbbe7986dae98f20aa7
SEA.Levie <svpv.hotro@gmail.com>
2025-05-06 17:01:10 +0700

    refactor: improve formatMoneyShort function and add negative value handling

--- d01/asm01.js
+++ d01/asm01.js
@@ -22,19 +22,37 @@ function countWords(str) {
     return str.length === 0 ? 0 : count;
 }
 function formatMoneyShort(value) {
-    if (value >= 1_000_000_000) return (value / 1_000_000_000).toFixed(2) + 'B';
-    if (value >= 1_000_000) return (value / 1_000_000).toFixed(2) + 'M';
-    if (value >= 1_000) return (value / 1_000).toFixed(2) + 'K';
-    return value.toString();
+    //check type
+    if (typeof value !== 'number') {
+        return '';
+    }
+    var isMinus = value < 0;
+    value = Math.abs(value);
+    var result = '';
+    if (value >= 1_000_000_000) {
+        result = (value / 1_000_000_000).toFixed(2) + 'B';
+    } else if (value >= 1_000_000) {
+        result = (value / 1_000_000).toFixed(2) + 'M';
+    } else if (value >= 1_000) {
+        result = (value / 1_000).toFixed(2) + 'K';
+    } else if (value < 1_000) {
+        result = value.toFixed(2);
+    }
+    if (isMinus) {
+        result = '-' + result;
+    }
+
+    return result;
 }
 function getExtension(filename) {
     const idx = filename.lastIndexOf(".");
     return idx !== -1 ? filename.slice(idx + 1) : "";
 }
 // test
-console.log("formatMoneyString(1234567890): " + formatMoneyString(1234567890)); // 1,234,567,890
-console.log("formatMoneyShort(1234567890): " + formatMoneyShort(1234567890)); // 1.23B
-console.log("countWords('Hello World!'): " + countWords('Hello World!')); // 2
-console.log("countWords('getHTTPResponse'): " + countWords('getHTTPResponse')); // 2
-console.log("getExtension('file.txt'): " + getExtension('file.txt')); // txt
-console.log("getExtension('file'): " + getExtension('file')); // ""
+console.log("formatMoneyString(1234567890): " + formatMoneyString(1234567890));
+console.log("formatMoneyString(1234567890): " + formatMoneyString(-1234567890)); 
+console.log("formatMoneyShort(1234567890): " + formatMoneyShort(-1234567890));
+console.log("countWords('Hello World!'): " + countWords('Hello World!')); 
+console.log("countWords('getHTTPResponse'): " + countWords('getHTTPResponse')); 
+console.log("getExtension('file.txt'): " + getExtension('file.txt')); 
+console.log("getExtension('file'): " + getExtension('file')); 
